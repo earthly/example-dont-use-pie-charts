da86da2afb72330482d0e44f6c735ff220ee8d29
Alex Couture-Beil <alex@earthly.dev>
2021-09-24 15:36:01 -0700

    code clean up
    Signed-off-by: Alex Couture-Beil <alex@mofo.ca>

--- dontusepiecharts/plot.py
+++ dontusepiecharts/plot.py
@@ -21,21 +21,21 @@ def process_data(data, labels, sort, reverse, percentage, format_percentage):
     return data, labels
 
 def plot_pie(data, labels, output_path):
-    fig = plt.figure(figsize=(10.0, 7.0), dpi=100)
+    fig = plt.figure(figsize=(5.0, 5.0), dpi=100)
     ax = fig.add_axes([0,0,1,1])
     ax.pie(data, labels=labels)
     fig.savefig(output_path, bbox_inches='tight')
 
 def plot_horizontal_bar(data, labels, output_path, sort=True, reverse=False, percentage=False, format_percentage=False):
     data, labels = process_data(data, labels, sort, reverse, percentage, format_percentage)
-    fig = plt.figure(figsize=(10.0, 5.0), dpi=100)
+    fig = plt.figure(figsize=(10.0, 3.0), dpi=100)
     ax = fig.add_axes([0,0,1,1])
     ax.barh(labels, data, height=0.1)
     fig.savefig(output_path, bbox_inches='tight')
 
 def plot_horizontal_lollipop(data, labels, output_path, sort=True, reverse=False, percentage=False, format_percentage=False, xlabel=None, ylabel=None, title=None):
     data, labels = process_data(data, labels, sort, reverse, percentage, format_percentage)
-    fig = plt.figure(figsize=(10.0, 7.0), dpi=100)
+    fig = plt.figure(figsize=(10.0, 3.0), dpi=100)
     ax = fig.add_axes([0,0,1,1])
     ax.set_title(title)
 
@@ -51,7 +51,7 @@ def plot_horizontal_lollipop(data, labels, output_path, sort=True, reverse=False
 
 def plot_horizontal_box_and_whisker(data, labels, output_path, sort=True, reverse=False, percentage=False, format_percentage=False, xlabel=None, ylabel=None, title=None, whis=None):
     data, labels = process_data(data, labels, sort, reverse, percentage, format_percentage)
-    fig = plt.figure(figsize=(10.0, 7.0), dpi=100)
+    fig = plt.figure(figsize=(10.0, 3.0), dpi=100)
     ax = fig.add_axes([0,0,1,1])
     ax.set_title(title)
     ax.set_xlabel(xlabel)
@@ -59,64 +59,3 @@ def plot_horizontal_box_and_whisker(data, labels, output_path, sort=True, revers
     data = numpy.array(data, dtype=object)
     ax.boxplot(data, labels=labels, vert=False, whis=whis)
     fig.savefig(output_path, bbox_inches='tight')
-
-#def plot_waffle(data, labels, output_path, width=10, height=10, sort=True, reverse=False, xlabel=None, ylabel=None, title=None, colormap='Dark2'):
-#    data, labels = process_data(data, labels, sort, reverse, True, False)
-#
-#    cmap = matplotlib.cm.get_cmap(colormap)
-#
-#    #waffle = [-1] * ()
-#    waffle = numpy.zeros((height,width))
-#    num_grids = height * width
-#    grids_per_label = [round(x * num_grids) for x in data]
-#
-#    # account for rounding errors
-#    while sum(grids_per_label) < num_grids:
-#        i = grids_per_label.index(max(grids_per_label))
-#        grids_per_label[i] += 1
-#    while sum(grids_per_label) < num_grids:
-#        i = grids_per_label.index(max(grids_per_label))
-#        grids_per_label[i] -= 1
-#
-#    label_i = 0
-#    for row in range(height):
-#        for col in range(width):
-#            i = row*width + col
-#            while label_i < len(labels) and grids_per_label[label_i] <= 0:
-#                label_i += 1
-#            if label_i >= len(labels):
-#                break
-#
-#            waffle[row,col] = label_i
-#            grids_per_label[label_i] -= 1
-#
-#    fig = plt.figure(figsize=(10.0, 7.0), dpi=100)
-#    ax = fig.add_axes([0,0,1,1])
-#    ax.set_title(title)
-#    ax.set_xlabel(xlabel)
-#    ax.set_ylabel(ylabel)
-#
-#    print(waffle)
-#    ax.imshow(waffle)
-#
-#    for i, label in enumerate(labels):
-#        color = cmap(i) # * 255 / len(labels))
-#        ax.plot(0, 0, "-", color=color, label=label)
-#
-#    ax.grid(which='minor', color='w', linestyle='-', linewidth=2)
-#    ax.tick_params(which='both', width=0, grid_alpha=0)
-#    ax.set_xticks(numpy.arange(-0.5, (width), 1), minor=True)
-#    ax.set_yticks(numpy.arange(-0.5, (height), 1), minor=True)
-#
-#    ax.legend() #loc='center left', bbox_to_anchor=(1, 0.5))
-#
-#    ax.xaxis.set_ticks([])
-#    ax.yaxis.set_ticks([])
-#
-#    #for row in range(height):
-#    #    for col in range(width):
-#    #        i = row*width + col
-#    #        label_i = waffle[]
-#    #        ax.imshow(row, col, color=cm(label_i))
-#    fig.savefig(output_path, bbox_inches='tight')
-
--- dontusepiecharts/plotprecipitation.py
+++ dontusepiecharts/plotprecipitation.py
@@ -1,11 +1,9 @@
-import math
 import re
 import os
 import pandas
 import datetime
-import matplotlib.pyplot as plt
-import matplotlib
-import matplotlib.pyplot as plt
+
+import plot
 
 # Our new date parser
 def yyyymmdd_parser(s):
@@ -49,55 +47,40 @@ def format_days_since_jan1(days, pos=None):
     date = datetime.date(2020, 1, 1) + datetime.timedelta(days)
     return date.strftime('%b')
 
-import matplotlib
-import matplotlib.pyplot as plt
 
-fig = plt.figure(figsize=(10.0, 7.0), dpi=100)
-ax = fig.add_axes([0, 0, 1, 1])
+if __name__ == '__main__':
+    weather = get_data('data')
+    weather['Total Precip (mm)'] = weather['Total Precip (mm)'].fillna(0)
+    weather['Total Snow (cm)'] = weather['Total Snow (cm)'].fillna(0)
+    rainfall_by_day_of_week = split_data_by_day_of_week(weather)
 
-weather = get_data('data')
-weather['Total Precip (mm)'] = weather['Total Precip (mm)'].fillna(0)
-weather['Total Snow (cm)'] = weather['Total Snow (cm)'].fillna(0)
-rainfall_by_day_of_week = split_data_by_day_of_week(weather)
+    total_rainfall_by_day_of_week = [sum(x) for x in rainfall_by_day_of_week]
 
-total_rainfall_by_day_of_week = [sum(x) for x in rainfall_by_day_of_week]
+    # Monday, Tuesday, ..., Sunday
+    day_names = days = [datetime.date(2001, 1, 1+i).strftime('%A') for i in range(7)]
 
-# Monday, Tuesday, ..., Sunday
-day_names = days = [datetime.date(2001, 1, 1+i).strftime('%A') for i in range(7)]
+    plot.plot_horizontal_lollipop(total_rainfall_by_day_of_week, day_names, 'rainfall-by-day-of-week.png', sort=False, xlabel='precipitation (mm)', ylabel='day of week', reverse=True, title='total precipitation by day of week')
 
-import plot
-plot.plot_horizontal_lollipop(total_rainfall_by_day_of_week, day_names, 'rainfall-by-day-of-week.png', sort=False, xlabel='precipitation (mm)', ylabel='day of week', reverse=True, title='total precipitation by day of week')
+    plot.plot_pie(total_rainfall_by_day_of_week, day_names, 'rainfall-by-day-of-week-pie-chart-is-hard-to-read.png')
 
-plot.plot_pie(total_rainfall_by_day_of_week, day_names, 'rainfall-by-day-of-week-pie-chart-is-hard-to-read.png')
+    plot.plot_horizontal_box_and_whisker(rainfall_by_day_of_week, day_names, 'rainfall-by-day-of-week-box-and-whisker-with-outliers.png',
+        sort=False, xlabel='precipitation (mm)', ylabel='day of week', reverse=True, title='precipitation by day of week (with outliers)', whis=None)
 
-plot.plot_horizontal_box_and_whisker(rainfall_by_day_of_week, day_names, 'rainfall-by-day-of-week-box-and-whisker-with-outliers.png',
-    sort=False, xlabel='precipitation (mm)', ylabel='day of week', reverse=True, title='precipitation by day of week (with outliers)', whis=None)
+    plot.plot_horizontal_box_and_whisker(rainfall_by_day_of_week, day_names, 'rainfall-by-day-of-week-box-and-whisker.png',
+        sort=False, xlabel='precipitation (mm)', ylabel='day of week', reverse=True, title='precipitation by day of week', whis=999999)
 
-plot.plot_horizontal_box_and_whisker(rainfall_by_day_of_week, day_names, 'rainfall-by-day-of-week-box-and-whisker.png',
-    sort=False, xlabel='precipitation (mm)', ylabel='day of week', reverse=True, title='precipitation by day of week', whis=999999)
+    amounts_when_raining = [
+        [x for x in amounts if x > 0]
+        for amounts in rainfall_by_day_of_week
+        ]
+    plot.plot_horizontal_box_and_whisker(amounts_when_raining, day_names, 'rainfall-by-day-of-week-box-and-whisker-when-raining.png',
+        sort=False, xlabel='precipitation (mm)', ylabel='day of week', reverse=True, title='amounts of precipitation for days when precipitation happened', whis=None)
 
-amounts_when_raining = [
-    [x for x in amounts if x > 0]
-    for amounts in rainfall_by_day_of_week
-    ]
-plot.plot_horizontal_box_and_whisker(amounts_when_raining, day_names, 'rainfall-by-day-of-week-box-and-whisker-when-raining.png',
-    sort=False, xlabel='precipitation (mm)', ylabel='day of week', reverse=True, title='amounts of precipitation for days when precipitation happened', whis=None)
-
-binary_raining_or_not_by_day_of_week = [
-    sum([1 for x in amounts if x > 0])
-    for amounts in rainfall_by_day_of_week
-    ]
-plot.plot_horizontal_lollipop(binary_raining_or_not_by_day_of_week, day_names, 'number-of-wet-days-by-day-of-week.png', sort=False, reverse=True, percentage=True,
-    title='distribution of days that experienced rain >= 1mm, values sum to 100%',
-    xlabel='percentage of rainy days',
-        )
-
-#plot.plot_waffle(binary_raining_or_not_by_day_of_week, day_names, 'waffle.png', sort=False)
-
-#chance_of_rain_by_day_of_week = [
-#    sum([1 for x in amounts if x > 10]) / len(amounts)
-#    for amounts in rainfall_by_day_of_week
-#    ]
-#plot.plot_horizontal_lollipop(chance_of_rain_by_day_of_week, day_names, 'chance-of-rain-by-day-of-week.png', sort=False, reverse=True, format_percentage=True,
-#    title='chance of rain given a particular day of the week',
-#        )
+    binary_raining_or_not_by_day_of_week = [
+        sum([1 for x in amounts if x > 0])
+        for amounts in rainfall_by_day_of_week
+        ]
+    plot.plot_horizontal_lollipop(binary_raining_or_not_by_day_of_week, day_names, 'number-of-wet-days-by-day-of-week.png', sort=False, reverse=True, percentage=True,
+        title='distribution of days that experienced rain >= 1mm, values sum to 100%',
+        xlabel='percentage of rainy days',
+            )
