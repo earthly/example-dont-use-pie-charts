552e3d56ae10852bf6a81bf6c0fcc2f45c953a35
Alex Couture-Beil <alex@earthly.dev>
2021-09-24 15:47:03 -0700

    fix Pac-Man spelling

--- dontusepiecharts/pacman.py
+++ dontusepiecharts/pacman.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     fig = plt.figure(figsize=(10.0, 4.5), dpi=100)
     ax = fig.add_axes([0,0,1,1])
     data = [90, 10]
-    labels = ['pie charts that look like pacman', 'pie charts that don\'t']
+    labels = ['pie charts that look like Pac-Man', 'pie charts that don\'t']
     ax.pie(data, labels=labels, colors=['#eeee33', '#4f4f4f'], startangle=20,
         textprops={
             'fontsize': 20,
